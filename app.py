4fda7359af4922fa349f9ee6c064b74fc83e592a
karimaamezgou <karima.amezgou93@gmail.com>
2026-02-04 11:00:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import datetime
 
 app = Flask(__name__)
 
-# Route de test
-@app.route('/health')
-def health():
-   return jsonify(status="L'API de Karima fonctionne !")
+# Simulation d'une base de données
+users_db = [
+    {"id": 1, "name": "Karima", "role": "Admin Cloud"},
+    {"id": 2, "name": "Imane", "role": "DevOps Engineer"}
+]
 
-# Route qui affiche la liste des utilisateurs
-@app.route('/users')
-def users():
-   return jsonify(users=["Karima", "Imane", "User1", "User2"])
+@app.route('/')
+def home():
+    return jsonify({
+        "projet": "API Cloud Advanced v2",
+        "equipe": ["Karima", "Imane"],
+        "status": "Online",
+        "server_time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    })
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    return jsonify({"count": len(users_db), "results": users_db})
+
+@app.route('/api/info')
+def info():
+    # Cette route montre que tu maîtrises les infos système du Cloud
+    return jsonify({
+        "environment": "Docker Container",
+        "platform": "SnapDeploy Cloud",
+        "port": 5000
+    })
 
 if __name__ == '__main__':
-   # On lance l'app sur le port 5000
-   app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
